39966a6defd6e21d8b7e3498a8cf6422fb6e7bb7
sqz269 <yin2690855381@gmail.com>
2019-11-03 11:41:40 -0600

    added documentation for fileContainerAddItemEx

--- fileshare/site/static/js/templates.js
+++ fileshare/site/static/js/templates.js
@@ -1,4 +1,15 @@
-function fileContainerAddItemEx(fname, fpath, size, lastMod, isDir, elementToAppend)
+/**
+ * Create a new file item from #file-template and add it too an element
+ * 
+ * @param {string} fname the file name
+ * @param {string} fpath file path
+ * @param {number} size the size of the file (cosmetic purpose only)
+ * @param {string} lastMod the last modification date of the file (cosmetic purpose only)
+ * @param {boolean} isDir is the file a directory (corresponding icons will be applies depend on the type)
+ * @param {Selector} elementToAppend a jquery selector that points to the table the element will be appended to
+ *                                   Default value is #file-container
+ */
+function fileContainerAddItemEx(fname, fpath, size, lastMod, isDir, elementToAppend="#file-container")
 {
     $templateElement = $("#file-template").clone(false);
     $templateElement.removeAttr("id");
@@ -15,9 +26,4 @@ function fileContainerAddItemEx(fname, fpath, size, lastMod, isDir, elementToApp
     $templateElement.find("#file-size").html(size).removeAttr("id");
     $templateElement.find("#file-operation-newtab").attr("href", fpath).removeAttr("id");
     $templateElement.appendTo(elementToAppend);
-}
-
-function fileContainerAddItem(fname, fpath, size, lastMod, isDir)
-{
-    fileContainerAddItemEx(fname, fpath, size, lastMod, isDir, "#file-container")
 }
