54c44c0cefc9084cd2b439e292577c2122f550c7
sqz269 <yin2690855381@gmail.com>
2020-01-19 00:26:01 -0600

    Added Type hint for some variables

--- fileshare/shared/database/Directory.py
+++ fileshare/shared/database/Directory.py
@@ -1,16 +1,16 @@
 from fileshare.shared.database.database import db
 
 class Directory(db.Model):
-    rel_path    = db.Column(db.Unicode(512), primary_key=True, nullable=False)
-    abs_path    = db.Column(db.Unicode(1024), unique=True, nullable=False)
-    parent_path = db.Column(db.Unicode(512))  # Can be nullable
-    name        = db.Column(db.Unicode(128), nullable=False)
-    content_dir = db.Column(db.Text)  # Sub-directories of the directory, names only, splitted by ,
-    content_file= db.Column(db.Text)  # Files in the directory, names only, splitted by ,
-    last_mod    = db.Column(db.Float)
-    dir_count   = db.Column(db.Integer)
-    file_count  = db.Column(db.Integer)
-    size        = db.Column(db.Integer, nullable=False)
+    rel_path: str     = db.Column(db.Unicode(512), primary_key=True, nullable=False)
+    abs_path: str     = db.Column(db.Unicode(1024), unique=True, nullable=False)
+    parent_path: str  = db.Column(db.Unicode(512))  # Can be nullable
+    name: str         = db.Column(db.Unicode(128), nullable=False)
+    content_dir: str  = db.Column(db.Text)  # Sub-directories of the directory, names only, splitted by ,
+    content_file: str = db.Column(db.Text)  # Files in the directory, names only, splitted by ,
+    last_mod: float   = db.Column(db.Float)
+    dir_count: int    = db.Column(db.Integer)
+    file_count: int   = db.Column(db.Integer)
+    size: int         = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return "<Name: %r; Path Abs: %r; Path Rel: %r; Size: %r;>" % (self.name, self.abs_path, self.rel_path, self.size)
--- fileshare/shared/database/File.py
+++ fileshare/shared/database/File.py
@@ -1,13 +1,13 @@
 from fileshare.shared.database.database import db
 
 class File(db.Model):
-    rel_path    = db.Column(db.Unicode(512), primary_key=True, nullable=False)
-    abs_path    = db.Column(db.Unicode(1024), unique=True, nullable=False)
-    parent_path = db.Column(db.Unicode(512), nullable=False)
-    name        = db.Column(db.Unicode(128), nullable=False)
-    last_mod    = db.Column(db.Float)
-    size        = db.Column(db.Integer)
-    mimetype    = db.Column(db.String(32))
+    rel_path: str    = db.Column(db.Unicode(512), primary_key=True, nullable=False)
+    abs_path: str    = db.Column(db.Unicode(1024), unique=True, nullable=False)
+    parent_path: str = db.Column(db.Unicode(512), nullable=False)
+    name: str        = db.Column(db.Unicode(128), nullable=False)
+    last_mod: float  = db.Column(db.Float)
+    size: int        = db.Column(db.Integer)
+    mimetype: str    = db.Column(db.String(32))
 
 
     def __repr__(self):
