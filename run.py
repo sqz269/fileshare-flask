4ee28cd49d25e0a27be236551067998de6cca715
sqz269 <yin2690855381@gmail.com>
2020-03-31 16:36:24 -0500

    make __name__ == __main__ not !=

--- run.py
+++ run.py
@@ -11,10 +11,10 @@ from fileshare.shared.database.init import init_db
 # CRITICAL 50
 app.logger.setLevel(10)
 
-if __name__ != "__main__":
-    app.config.from_object("config_local.ConfigProduction")
-else:
+if __name__ == "__main__":
     app.config.from_object("config_local.ConfigTesting")
+else:
+    app.config.from_object("config_local.ConfigProduction")
 
 db.init_app(app)
 init_db()
